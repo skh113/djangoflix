8ccf3b7b04b7912fd76e7fd714ed9854a19546b8
Keyvan <skhprogramming@gmail.com>
2025-02-07 13:38:50 +0330

    test: added basic tests to movie app

--- src/videos/tests.py
+++ src/videos/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Video
+
+
+class VideoModelTestCase(TestCase):
+    MOVIE_TITLE = 'A movie title for testing'
+
+    def setUp(self) -> None:
+        Video.objects.create(title=self.MOVIE_TITLE)
+
+    def test_valid_title(self):
+        query_set = Video.objects.filter(title=self.MOVIE_TITLE)
+        self.assertTrue(query_set.exists())
+
+    def test_created_count(self):
+        query_set = Video.objects.all()
+        self.assertEqual(query_set.count(), 1)
