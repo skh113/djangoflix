57ba595734abf9b585a383ef13dfeadfdf637b4e
Keyvan <skhprogramming@gmail.com>
2025-02-07 13:30:43 +0330

    feat: made video model migrations

--- src/videos/models.py
+++ src/videos/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Video(models.Model):
-    title = models.CharField()
-    description = models.TextField()
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
     slug = models.SlugField(blank=True, null=True)
-    video_id = models.CharField()
+    video_id = models.CharField(max_length=255)
     # timestamp
     # updated
     # state
