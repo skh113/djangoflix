082ffb9ca78957aabe7dfc609a78c36e33e3b599
Keyvan <skhprogramming@gmail.com>
2025-02-08 16:09:44 +0330

    feat: added proxy models in admin

--- src/videos/admin.py
+++ src/videos/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import VideoAllProxy, VideoPublishedProxy
+
+
+class VideoAllAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    list_filter = ['video_id']
+    search_fields = ['title', 'description', 'video_id']
+
+    class Meta:
+        model = VideoAllProxy
+
+
+admin.site.register(VideoAllProxy, VideoAllAdmin)
+
+
+class VideoProxyAdmin(admin.ModelAdmin):
+    list_display = ['title', 'video_id']
+    list_filter = ['video_id']
+    search_fields = ['title', 'description', 'video_id']
+
+    class Meta:
+        model = VideoPublishedProxy
+
+    def get_queryset(self, request):
+        return VideoPublishedProxy.objects.filter(is_active=True)
+
+
+admin.site.register(VideoPublishedProxy, VideoProxyAdmin)
--- src/videos/models.py
+++ src/videos/models.py
@@ -1,12 +1,44 @@
 from django.db import models
+from django.utils.text import slugify
 
 
 class Video(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(unique=True, max_length=255, blank=True, null=True)
     video_id = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+
     # timestamp
     # updated
     # state
     # publish_timestamp
+
+    def save(self, *args, **kwargs):
+        if not self.slug:  # If slug is not already provided
+            self.slug = slugify(self.title)  # Generate slug from title
+
+        # Ensure unique slug
+        original_slug = self.slug
+        counter = 1
+        while Video.objects.filter(slug=self.slug).exists():
+            self.slug = f"{original_slug}-{counter}"
+            counter += 1
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class VideoAllProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'All Video'
+        verbose_name_plural = 'All Videos'
+
+
+class VideoPublishedProxy(Video):
+    class Meta:
+        proxy = True
+        verbose_name = 'Published Video'
+        verbose_name_plural = 'Published Videos'
